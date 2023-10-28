1ca7fb634897ec977c454831e6a1f23f9b2c638e
Ruben <ruben.fileti.3@gmail.com>
2023-10-28 14:21:47 +0200

    add filter reset button

--- src/components/transaction/transaction-filters.tsx
+++ src/components/transaction/transaction-filters.tsx
@@ -14,64 +14,57 @@ function valuetext(value: number) {
 }
 
 export default function TransactionFilters() {
+  const [description, setDescription] = useState<string>('');
   const [amount, setAmount] = useState<number[]>([minAmountValue, maxAmountValue]);
   const [date, setDate] = useState<string[]>(['', '']);
+  const [dateDisplay, setDateDisplay] = useState<(Dayjs|null)[]>([null, null])
 
-  const handleAmountSliderChange = (event: Event, newValue: number | number[], activeThumb: number) => {
+  const onAmountSliderChange = (event: Event, newValue: number | number[], activeThumb: number) => {
     if (!Array.isArray(newValue)) {
       return;
     }
 
     if (activeThumb === 0) {
-      changeMinAmount(newValue[0]);
+      setAmount([Math.min(newValue[0], amount[1] - minDistance), amount[1]]);
     } else {
-      changeMaxAmount(newValue[1]);
+      setAmount([amount[0], Math.max(newValue[1], amount[0] + minDistance)]);
     }
   };
 
-  const handleAmountMinChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const newValue = event.target.value === '' ? 0 : Number(event.target.value);
-    changeMinAmount(isNaN(newValue) ? 0 : newValue);
+  const onAmountMinChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    let newValue = event.target.value === '' ? 0 : Number(event.target.value);
+    newValue = isNaN(newValue) ? maxAmountValue : newValue;
+    setAmount([Math.min(newValue, amount[1] - minDistance), amount[1]]);
   };
 
-  const handleAmountMaxChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const newValue = event.target.value === '' ? minAmountValue : Number(event.target.value);
-    changeMaxAmount(isNaN(newValue) ? maxAmountValue : newValue);
-  }
-
-  const changeMinAmount = (newValue: number) => {
-    setAmount([Math.min(newValue, amount[1] - minDistance), amount[1]]);
-  }
-
-  const changeMaxAmount = (newValue: number) => {
+  const onAmountMaxChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    let newValue = event.target.value === '' ? minAmountValue : Number(event.target.value);
+    newValue = isNaN(newValue) ? maxAmountValue : newValue;
     setAmount([amount[0], Math.max(newValue, amount[0] + minDistance)]);
   }
 
-  const handleFromDateChange = (newValue: any, context: PickerChangeHandlerContext<DateValidationError>) => {
-    setDate([newValue.format('YYYY-MM-DD'), date[1]]);
+  const resetFilters = () => {
+    setDescription('');
+    setAmount([minAmountValue, maxAmountValue]);
+    setDateDisplay([null, null]);
+    setDate(['', '']);
   }
-
-  const handleToDateChange = (newValue: any, context: PickerChangeHandlerContext<DateValidationError>) => {
-    setDate([date[0], newValue.format('YYYY-MM-DD')]);
-  }
-
-
 
   return (
     <Box>
       <Form>
         <Stack spacing={4}>
           <Box>
-            <TextField id="description" name="description" label="Description" variant="outlined" />
+            <TextField value={description} id="description" name="description" label="Description" variant="outlined" onChange={(e) => setDescription(e.target.value)} />
           </Box>
           <Box>
-            <Typography sx={{mb: 1.5}}>Amount:</Typography>
+            <Typography sx={{ mb: 1.5 }}>Amount:</Typography>
             <Grid container >
               <Grid item xs={12} md={6} sx={{ paddingRight: '8px' }} >
-                <TextField id="amount_min" value={amount[0]} name="amount_min" label="Min" variant="outlined" onChange={handleAmountMinChange} />
+                <TextField id="amount_min" value={amount[0]} name="amount_min" label="Min" variant="outlined" onChange={onAmountMinChange} />
               </Grid>
               <Grid item xs={12} md={6} sx={{ paddingLeft: '8px' }}>
-                <TextField id="amount_max" value={amount[1]} name="amount_max" label="Max" variant="outlined" onChange={handleAmountMaxChange} />
+                <TextField id="amount_max" value={amount[1]} name="amount_max" label="Max" variant="outlined" onChange={onAmountMaxChange} />
               </Grid>
             </Grid>
             <Slider
@@ -79,11 +72,12 @@ export default function TransactionFilters() {
                 '& .MuiSlider-markLabel': {
                   transform: 'translateX(-100%)'
                 },
-                mt: 2
+                mt: 2,
+                mb: 0
               }}
               getAriaLabel={() => 'Minimum distance'}
               value={amount}
-              onChange={handleAmountSliderChange}
+              onChange={onAmountSliderChange}
               valueLabelDisplay="auto"
               getAriaValueText={valuetext}
               disableSwap
@@ -102,19 +96,32 @@ export default function TransactionFilters() {
             />
           </Box>
           <Box>
-            <Typography sx={{mb: 1.5}}>Date:</Typography>
+            <Typography sx={{ mb: 1.5 }}>Date:</Typography>
             <Grid container >
               <Grid item xs={12} md={6} sx={{ paddingRight: '8px' }} >
-                <DatePicker onChange={handleFromDateChange} />
+                <DatePicker
+                  value={dateDisplay[0]} 
+                  onChange={(v: any) => { setDate([v.format('YYYY-MM-DD'), date[1]]); setDateDisplay([v, dateDisplay[1]])} } />
                 <input type="hidden" name="from_date" value={date[0]} />
               </Grid>
               <Grid item xs={12} md={6} sx={{ paddingLeft: '8px' }}>
-                <DatePicker onChange={handleToDateChange} />
+                <DatePicker
+                  value={dateDisplay[1]}
+                  onChange={(v: any) => {setDate([date[0], v.format('YYYY-MM-DD')]); setDateDisplay([dateDisplay[0], v]); }} />
                 <input type="hidden" name="to_date" value={date[1]} />
               </Grid>
             </Grid>
           </Box>
-          <Button type="submit" variant="outlined" sx={{ padding: '16px 32px', alignSelf: 'center', width: 'fit-content' }}>Apply</Button>
+          <Box>
+            <Grid container spacing={3}>
+              <Grid item xs={6}>
+                <Button type="button" onClick={resetFilters} variant="outlined" color='error' sx={{ padding: '16px 32px', alignSelf: 'center', width: '100%' }}>Reset</Button>
+              </Grid>
+              <Grid item xs={6}>
+                <Button type="submit" variant="outlined" sx={{ padding: '16px 32px', alignSelf: 'center', width: '100%' }}>Apply</Button>
+              </Grid>
+            </Grid>
+          </Box>
         </Stack>
       </Form>
     </Box>
