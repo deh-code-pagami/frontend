cd95dcb3f7322aa51e6a78b128248a61eb5d64ae
Ruben <ruben.fileti.3@gmail.com>
2023-11-04 23:49:44 +0100

    Implement persistent drawer for desktop

--- src/components/drawer/drawer.tsx
+++ src/components/drawer/drawer.tsx
@@ -4,12 +4,45 @@ import Button from '@mui/material/Button';
 import SwipeableDrawer from '@mui/material/SwipeableDrawer';
 import * as React from 'react';
 import MainNavigation from '../navigation/navigation';
+import { Container, Drawer } from '@mui/material';
 
-export default function Drawer() {
+const drawerWidth = 250;
+
+
+function DrawerContent() {
+  return (
+    <Box
+      sx={{ width: drawerWidth }}
+      role="presentation"
+    >
+      <MainNavigation></MainNavigation>
+    </Box>
+  )
+}
+
+export default function MainDrawer() {
+  return (
+    <Drawer
+      variant="permanent"
+      sx={{
+        display: { xs: 'none', xl: 'block' },
+        '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth, position: 'static', overflow: 'hidden'},
+      }}
+      open
+    >
+      <DrawerContent />
+    </Drawer>
+  )
+}
+
+export function MainDrawerMobile(props: { window?: () => Window }) {
+  const { window } = props;
 
   const [state, setState] = React.useState({ open: false })
 
-  const toggleDrawer = (open: boolean) =>
+  const container = window !== undefined ? () => window().document.body : undefined;
+
+  const toggleDrawer = (open: boolean) => (
     (event: React.KeyboardEvent | React.MouseEvent) => {
       if (
         event &&
@@ -21,29 +54,40 @@ export default function Drawer() {
       }
 
       setState({ ...state, open });
-    };
-
+    }
+  )
 
   return (
-    <div>
-      <React.Fragment>
-        <Button onClick={toggleDrawer(true)}><MenuIcon /></Button>
-        <SwipeableDrawer
-          anchor={'left'}
-          open={state.open}
-          onClose={toggleDrawer(false)}
-          onOpen={toggleDrawer(true)}
-        >
-          <Box
-          sx={{ width: 250 }}
-          role="presentation"
-          onClick={toggleDrawer(false)}
-          onKeyDown={toggleDrawer(false)}
+    <>
+      <Box sx={{
+        boxShadow: '0 2px 8px 0px #00000030',
+        backgroundColor: 'rgb(251,251,251)',
+        background: 'radial-gradient(circle, rgba(251,251,251,1) 0%, rgba(240,240,240,1) 100%)',
+        display: { xs: 'block', xl: 'none' }
+      }}>
+        <Container sx={{
+          paddingY: '16px'
+        }}>
+          <Button onClick={toggleDrawer(true)}><MenuIcon /></Button>
+          <SwipeableDrawer
+            anchor={'left'}
+            open={state.open}
+            onClose={toggleDrawer(false)}
+            onOpen={toggleDrawer(true)}
+            container={container}
+            variant="temporary"
+            ModalProps={{
+              keepMounted: true, // Better open performance on mobile.
+            }}
+            sx={{
+              display: { xs: 'block', xl: 'none' },
+              '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+            }}
           >
-            <MainNavigation></MainNavigation>
-          </Box>
-        </SwipeableDrawer>
-      </React.Fragment>
-    </div>
+            <DrawerContent />
+          </SwipeableDrawer>
+        </Container>
+      </Box>
+    </>
   );
 }
--- src/pages/root.tsx
+++ src/pages/root.tsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router-dom";
-import Drawer from "../components/drawer/drawer";
+import MainDrawer, { MainDrawerMobile } from "../components/drawer/drawer";
 import { Box, Container } from "@mui/material";
 
 
@@ -7,19 +7,10 @@ export default function Root() {
   return (
     <>
       <header id="main-header">
-        <Box sx={{
-          boxShadow: '0 2px 8px 0px #00000030',
-          backgroundColor: 'rgb(251,251,251)',
-          background: 'radial-gradient(circle, rgba(251,251,251,1) 0%, rgba(240,240,240,1) 100%)'
-        }}>
-          <Container sx={{
-            paddingY: '16px'
-          }}>
-            <Drawer></Drawer>
-          </Container>
-        </Box>
+        <MainDrawerMobile/>
       </header>
       <main id="main-content">
+        <MainDrawer/>
         <Outlet />
       </main>
       <footer id="main-footer"></footer>
